253442acaf3e02e0a0f24175857343cfccaa5f51
Joon Ho Cho <joonho1101@gmail.com>
2018-10-27 00:03:22 -0700

    formatShortNumber

--- src/number.test.ts
+++ src/number.test.ts
@@ -50,13 +50,21 @@ test('formatShortNumber', () => {
   expect(formatShortNumber(0)).toBe('0');
   expect(formatShortNumber(999)).toBe('999');
   expect(formatShortNumber(1000)).toBe('1.0K');
+  expect(formatShortNumber(1499)).toBe('1.5K');
   expect(formatShortNumber(1000, { precision: 1 })).toBe('1K');
-  expect(formatShortNumber(10000)).toBe('10K');
+  expect(formatShortNumber(12345)).toBe('12K');
   expect(
-    formatShortNumber(10000, { divisor: 10000, units: ['', 'm'], precision: 3 })
-  ).toBe('1.00m');
-  expect(formatShortNumber(99999)).toBe('100K');
-  expect(formatShortNumber(999999)).toBe('1.0M');
+    formatShortNumber(12345, { divisor: 10000, units: ['', 'm'], precision: 3 })
+  ).toBe('1.23m');
+  expect(formatShortNumber(123456)).toBe('124K');
+  expect(formatShortNumber(123456, { round: 'floor' })).toBe('123K');
+  expect(formatShortNumber(123456, { round: 'ceil' })).toBe('124K');
+  expect(formatShortNumber(123456, { precision: 1 })).toBe('123K');
+  expect(formatShortNumber(123456, { precision: 2 })).toBe('124K');
+  expect(formatShortNumber(123456, { precision: 3 })).toBe('123K');
+  expect(formatShortNumber(123456, { precision: 4 })).toBe('123.5K');
+  expect(formatShortNumber(123456, { precision: 5 })).toBe('123.46K');
+  expect(formatShortNumber(1234567)).toBe('1.2M');
   expect(formatShortNumber(1000000)).toBe('1.0M');
   expect(formatShortNumber(10000000)).toBe('10M');
   expect(formatShortNumber(100000000)).toBe('100M');
--- src/number.ts
+++ src/number.ts
@@ -37,10 +37,12 @@ export const formatShortNumber = (
     divisor = 1000,
     units = defaultSuffixes,
     precision = 2,
+    round = 'round',
   }: {
     divisor?: number;
     units?: string[];
     precision?: number;
+    round?: 'round' | 'floor' | 'ceil';
   } = {}
 ): string => {
   const sign = n >= 0 ? '' : '-';
@@ -48,11 +50,13 @@ export const formatShortNumber = (
   const len = units.length;
   for (let i = 0; i < len; i += 1) {
     if (abs < divisor || i >= len - 1) {
-      const p = Math.max(0, precision - `${abs}`.length);
+      const sigN = `${abs}`.replace(/\..*$/, '').length;
+      const p = Math.max(0, precision - sigN);
       const ns = i === 0 ? abs : abs.toFixed(p);
       return `${sign}${ns}${units[i]}`;
     }
-    abs = Math.round(abs / divisor);
+    const pp = Math.pow(10, Math.max(0, precision - 1));
+    abs = Math[round]((abs * pp) / divisor) / pp;
   }
   return `${sign}${abs}${units[len - 1]}`;
 };
