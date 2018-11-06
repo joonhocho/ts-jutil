4457cf18e315e97b183db5d6e5e66ee2d445bf69
Joon Ho Cho <joonho1101@gmail.com>
2018-11-06 10:25:32 -0800

    parse youtube video url

---
+++ src/youtube.test.ts
@@ -0,0 +1,39 @@
+import { parseVideoIdFromYoutubeVideoUrl } from './youtube';
+
+test('parseVideoIdFromYoutubeVideoUrl', () => {
+  expect(
+    parseVideoIdFromYoutubeVideoUrl(
+      'https://www.youtube.com/watch?v=tOpoyyEAI-o'
+    )
+  ).toBe('tOpoyyEAI-o');
+  expect(
+    parseVideoIdFromYoutubeVideoUrl(
+      'https://www.youtube.com/watch?v=tOpoyyEAI-o&t=1'
+    )
+  ).toBe('tOpoyyEAI-o');
+  expect(
+    parseVideoIdFromYoutubeVideoUrl('youtube.com/watch?v=tOpoyyEAI-o')
+  ).toBe('tOpoyyEAI-o');
+
+  expect(parseVideoIdFromYoutubeVideoUrl('https://youtu.be/tOpoyyEAI-o')).toBe(
+    'tOpoyyEAI-o'
+  );
+  expect(
+    parseVideoIdFromYoutubeVideoUrl('https://youtu.be/tOpoyyEAI-o&t=1')
+  ).toBe('tOpoyyEAI-o');
+  expect(parseVideoIdFromYoutubeVideoUrl('youtu.be/tOpoyyEAI-o')).toBe(
+    'tOpoyyEAI-o'
+  );
+
+  expect(
+    parseVideoIdFromYoutubeVideoUrl('https://www.youtube.com/embed/tOpoyyEAI-o')
+  ).toBe('tOpoyyEAI-o');
+  expect(
+    parseVideoIdFromYoutubeVideoUrl(
+      'https://www.youtube.com/embed/tOpoyyEAI-o&t=1'
+    )
+  ).toBe('tOpoyyEAI-o');
+  expect(parseVideoIdFromYoutubeVideoUrl('youtube.com/embed/tOpoyyEAI-o')).toBe(
+    'tOpoyyEAI-o'
+  );
+});
