83b72e6f42ff6e81b913502db52b8472d339e84d
Joon Ho Cho <joonho1101@gmail.com>
2019-06-12 05:43:37 -0700

    add more youtube functions

--- src/youtube.test.ts
+++ src/youtube.test.ts
@@ -1,46 +1,77 @@
-import { parseVideoIdFromYouTubeVideoUrl } from './youtube';
+import {
+  getVideoThumbnailUrl,
+  getYouTubeChannelUrl,
+  getYouTubeVideoEmbedUrl,
+  getYouTubeVideoUrl,
+  parseVideoIdFromYouTubeVideoUrl,
+} from './youtube';
+
+const videoId = 'pCJZg7pVtiI';
+const channelId = 'UCH3sMKVJ2P7hm7ROBy5FHJga';
+
+test('getVideoThumbnailUrl', () => {
+  expect(getVideoThumbnailUrl(videoId, 'hqdefault')).toBe(
+    `http://i3.ytimg.com/vi/${videoId}/hqdefault.jpg`
+  );
+});
+
+test('getYouTubeChannelUrl', () => {
+  expect(getYouTubeChannelUrl(channelId)).toBe(
+    `https://www.youtube.com/channel/${channelId}`
+  );
+});
+
+test('getYouTubeVideoEmbedUrl', () => {
+  expect(getYouTubeVideoEmbedUrl(videoId)).toBe(
+    `https://www.youtube.com/embed/${videoId}`
+  );
+});
+
+test('getYouTubeVideoUrl', () => {
+  expect(getYouTubeVideoUrl(videoId)).toBe(
+    `https://www.youtube.com/watch?v=${videoId}`
+  );
+});
 
 test('parseVideoIdFromYouTubeVideoUrl', () => {
   expect(
     parseVideoIdFromYouTubeVideoUrl(
-      'https://www.youtube.com/watch?v=tOpoyyEAI-o'
+      `https://www.youtube.com/watch?v=${videoId}`
     )
-  ).toBe('tOpoyyEAI-o');
+  ).toBe(videoId);
   expect(
     parseVideoIdFromYouTubeVideoUrl(
-      'https://www.youtube.com/watch?v=tOpoyyEAI-o&t=1'
+      `https://www.youtube.com/watch?v=${videoId}&t=1`
     )
-  ).toBe('tOpoyyEAI-o');
+  ).toBe(videoId);
   expect(
-    parseVideoIdFromYouTubeVideoUrl('youtube.com/watch?v=tOpoyyEAI-o')
-  ).toBe('tOpoyyEAI-o');
+    parseVideoIdFromYouTubeVideoUrl(`youtube.com/watch?v=${videoId}`)
+  ).toBe(videoId);
   expect(
-    parseVideoIdFromYouTubeVideoUrl('iyoutube.com/watch?v=tOpoyyEAI-o')
+    parseVideoIdFromYouTubeVideoUrl(`iyoutube.com/watch?v=${videoId}`)
   ).toBe(null);
 
-  expect(parseVideoIdFromYouTubeVideoUrl('https://youtu.be/tOpoyyEAI-o')).toBe(
-    'tOpoyyEAI-o'
+  expect(parseVideoIdFromYouTubeVideoUrl(`https://youtu.be/${videoId}`)).toBe(
+    videoId
   );
   expect(
-    parseVideoIdFromYouTubeVideoUrl('https://youtu.be/tOpoyyEAI-o&t=1')
-  ).toBe('tOpoyyEAI-o');
-  expect(parseVideoIdFromYouTubeVideoUrl('youtu.be/tOpoyyEAI-o')).toBe(
-    'tOpoyyEAI-o'
-  );
-  expect(parseVideoIdFromYouTubeVideoUrl('iyoutu.be/tOpoyyEAI-o')).toBe(null);
+    parseVideoIdFromYouTubeVideoUrl(`https://youtu.be/${videoId}&t=1`)
+  ).toBe(videoId);
+  expect(parseVideoIdFromYouTubeVideoUrl(`youtu.be/${videoId}`)).toBe(videoId);
+  expect(parseVideoIdFromYouTubeVideoUrl(`iyoutu.be/${videoId}`)).toBe(null);
 
   expect(
-    parseVideoIdFromYouTubeVideoUrl('https://www.youtube.com/embed/tOpoyyEAI-o')
-  ).toBe('tOpoyyEAI-o');
+    parseVideoIdFromYouTubeVideoUrl(`https://www.youtube.com/embed/${videoId}`)
+  ).toBe(videoId);
   expect(
     parseVideoIdFromYouTubeVideoUrl(
-      'https://www.youtube.com/embed/tOpoyyEAI-o&t=1'
+      `https://www.youtube.com/embed/${videoId}&t=1`
     )
-  ).toBe('tOpoyyEAI-o');
-  expect(parseVideoIdFromYouTubeVideoUrl('youtube.com/embed/tOpoyyEAI-o')).toBe(
-    'tOpoyyEAI-o'
+  ).toBe(videoId);
+  expect(parseVideoIdFromYouTubeVideoUrl(`youtube.com/embed/${videoId}`)).toBe(
+    videoId
   );
-  expect(
-    parseVideoIdFromYouTubeVideoUrl('iyoutube.com/embed/tOpoyyEAI-o')
-  ).toBe(null);
+  expect(parseVideoIdFromYouTubeVideoUrl(`iyoutube.com/embed/${videoId}`)).toBe(
+    null
+  );
 });
--- src/youtube.ts
+++ src/youtube.ts
@@ -1,3 +1,24 @@
+export type YouTubeVideoThumbnailSize =
+  | 'default'
+  | 'hqdefault'
+  | 'mqdefault'
+  | 'sddefault'
+  | 'maxresdefault';
+
+export const getVideoThumbnailUrl = (
+  videoId: string,
+  size: YouTubeVideoThumbnailSize
+): string => `http://i3.ytimg.com/vi/${videoId}/${size}.jpg`;
+
+export const getYouTubeChannelUrl = (channelId: string): string =>
+  `https://www.youtube.com/channel/${channelId}`;
+
+export const getYouTubeVideoUrl = (videoId: string): string =>
+  `https://www.youtube.com/watch?v=${videoId}`;
+
+export const getYouTubeVideoEmbedUrl = (videoId: string): string =>
+  `https://www.youtube.com/embed/${videoId}`;
+
 // https://www.youtube.com/watch?v=tOpoyyEAI-o
 // https://youtu.be/tOpoyyEAI-o
 // https://www.youtube.com/embed/tOpoyyEAI-o
