8951d2ae90302d22f642e1de644e54e0cc82b365
Joon Ho Cho <joonho1101@gmail.com>
2019-12-23 22:35:38 +0900

    Value: async listener

--- src/Value.test.ts
+++ src/Value.test.ts
@@ -46,4 +46,73 @@ describe('Value', () => {
     ]);
     expect(vs3).toEqual([[7, 6]]);
   });
+
+  test('async', () => {
+    jest.useFakeTimers();
+
+    const value = new Value<number>(0);
+
+    const vs1: Array<[number, number]> = [];
+    const off1 = value.on((v, pv) => vs1.push([v, pv]));
+
+    const vs2: Array<[number, number]> = [];
+    const off2 = value.on((v, pv) => vs2.push([v, pv]), { async: true });
+
+    value.set(1);
+    value.set(1);
+    value.set(2);
+    value.set(3);
+
+    expect(vs1).toEqual([
+      [1, 0],
+      [2, 1],
+      [3, 2],
+    ]);
+    expect(vs2).toEqual([]);
+
+    jest.runAllTimers();
+
+    expect(vs2).toEqual([[3, 0]]);
+
+    vs1.length = vs2.length = 0;
+
+    value.set(1);
+    value.set(1);
+    value.set(2);
+    value.set(3);
+
+    expect(vs1).toEqual([
+      [1, 3],
+      [2, 1],
+      [3, 2],
+    ]);
+    expect(vs2).toEqual([]);
+
+    jest.runAllTimers();
+    expect(vs2).toEqual([]);
+
+    vs1.length = vs2.length = 0;
+
+    off1();
+
+    value.set(1);
+
+    expect(vs1).toEqual([]);
+    expect(vs2).toEqual([]);
+
+    jest.runAllTimers();
+
+    expect(vs1).toEqual([]);
+    expect(vs2).toEqual([[1, 3]]);
+
+    vs1.length = vs2.length = 0;
+
+    off2();
+
+    value.set(2);
+    jest.runAllTimers();
+
+    expect(vs1).toEqual([]);
+    expect(vs2).toEqual([]);
+  });
 });
--- src/Value.ts
+++ src/Value.ts
@@ -1,18 +1,25 @@
 export type ValueListener<T> = (value: T, prevValue: T) => void;
 
+export type Updater<T> = (v: T) => T;
+
+export interface IListenerOptions {
+  async?: boolean;
+}
+
 export class Value<T> {
   private fns: Array<ValueListener<T>> = [];
+  private asyncFns: Array<ValueListener<T>> = [];
+  private asyncQueued = false;
 
   constructor(protected _value: T) {}
 
-  public on(fn: ValueListener<T>): () => boolean {
-    const { fns } = this;
-    fns.push(fn);
-    return (): boolean => this.off(fn);
+  public on(fn: ValueListener<T>, opts?: IListenerOptions): () => boolean {
+    (opts && opts.async ? this.asyncFns : this.fns).push(fn);
+    return (): boolean => this.off(fn, opts);
   }
 
-  public off(fn: ValueListener<T>): boolean {
-    const { fns } = this;
+  public off(fn: ValueListener<T>, opts?: IListenerOptions): boolean {
+    const fns = opts && opts.async ? this.asyncFns : this.fns;
     const i = fns.indexOf(fn);
     if (i >= 0) {
       fns.splice(i, 1);
@@ -29,19 +36,40 @@ export class Value<T> {
     return this._value;
   }
 
-  public set(value: T): void {
-    const { _value } = this;
-    if (value !== _value) {
-      this._value = value;
-      const fns = this.fns.slice();
-      const len = fns.length;
-      for (let i = 0; i < len; i += 1) {
-        fns[i](value, _value);
+  public set(value: T | Updater<T>): void {
+    const oldValue = this._value;
+    const newValue =
+      typeof value === 'function' ? (value as Updater<T>)(oldValue) : value;
+
+    if (newValue !== oldValue) {
+      this._value = newValue;
+      const len = this.fns.length;
+      if (len > 0) {
+        for (let i = 0, fns = this.fns.slice(); i < len; i += 1) {
+          fns[i](newValue, oldValue);
+        }
+      }
+
+      if (!this.asyncQueued && this.asyncFns.length) {
+        this.asyncQueued = true;
+        setTimeout(() => {
+          this.asyncQueued = false;
+          const newValueAsync = this._value;
+          if (newValueAsync !== oldValue) {
+            for (
+              let ai = 0, afns = this.asyncFns.slice(), alen = afns.length;
+              ai < alen;
+              ai += 1
+            ) {
+              afns[ai](newValueAsync, oldValue);
+            }
+          }
+        }, 0);
       }
     }
   }
 
   public clear(): void {
-    this.fns.length = 0;
+    this.fns.length = this.asyncFns.length = 0;
   }
 }
